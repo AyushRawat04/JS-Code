e8333e5c7ca4d2f3152182607a4b867e83ed1e0c
java13815792 <java13815792@ldap.com>
2025-04-29 04:51:22 +0530

    Var,const,let

---
+++ 01_Basic/01_Variable.js
@@ -0,0 +1,24 @@
+const accountID = 191302013
+let accountEmail = "ayush@accenture.com"
+var accountPassward = "123456"
+accountCity = "Jaipur"
+let accountState;
+
+console.log(accountID)
+console.log(accountEmail)
+console.log(accountPassward)
+console.log(accountCity)
+console.log(accountState)
+
+
+
+// accountID = 123 Not allowed
+accountEmail = "aayush@accenture.com" 
+accountPassward = "7890"
+accountCity = "Delhi"
+console.table([accountID,accountEmail,accountPassward,accountCity,accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
